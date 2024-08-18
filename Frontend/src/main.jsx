6870efde5d255d73b9c830ba31a55170113dca37
Bl1tz2200 <154754925+Bl1tz2200@users.noreply.github.com>
2024-08-18 20:23:11 +0300

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { AuthProvider } from './Providers/AuthProvider.jsx';
 import { Routes } from './Routers/routes.jsx';
 
-export const API_ADRESS = "http://127.0.0.1:8080" // Enter here your frontend url (without / at the end)
+export const API_ADRESS = "http://127.0.0.1:8080" // Enter here your backend url (without / at the end)
 
 ReactDOM.createRoot(document.getElementById('root')).render( // Render all pages
   <React.StrictMode>
