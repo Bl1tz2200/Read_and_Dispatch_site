08ae69b016088e1a9541240a48ddace86364b8d2
Bl1tz2200 <154754925+Bl1tz2200@users.noreply.github.com>
2024-08-18 20:33:22 +0300

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Change here your port to run
+    host: "localhost", // Change here your running ip
+    port: 5173, // Change here your running port
   }
 })
